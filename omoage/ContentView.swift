85623a146fddad7dd56bb4338b67c2bf94bf7efc
takahori <kakikori224@gmaill.com>
2026-03-11 14:19:54 +0900

    Initial Commit

---
+++ omoage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  omoage
+//
+//  Created by takashi on 2026/03/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ omoage/omoageApp.swift
@@ -0,0 +1,17 @@
+//
+//  omoageApp.swift
+//  omoage
+//
+//  Created by takashi on 2026/03/11.
+//
+
+import SwiftUI
+
+@main
+struct omoageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
